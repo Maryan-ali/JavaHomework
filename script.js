23608c06f70662143e35ee24e9e3394892707eb6
Maryan Ali <maryan.ali131@gmail.com>
2022-03-06 23:48:06 -0600

    index.html

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var allowUppercase;
 var allowLowercase;
 var allowNumbers;
-var allowspecials;
+var allowSpecials;
 var passwordCharacterCount;
 
 var allowUppercaseChars=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -17,6 +17,25 @@ function askAboutUppercase() {
   console.log(allowUpercase);
 }
 
+function askAboutNumbers() {
+  allowNumbers=confirm("Are number characters allowed?");
+  console.log(allowNumbers);
+}
+
+function askAboutLowercase() {
+  allowLowercase=confirm("Are lowercase characters allowed?");
+  console.log(allowLowercase);
+}
+
+function askAboutSpecials() {
+  allowSpecials=confirm("Are special characters allowed?");
+  console.log(allowSpecials);
+}
+
+function askAboutpasswordCount() {
+  allowpasswordCharacterCount=confirm("What should be the character count allowed?");
+  console.log(allowNumbers);
+}
 //Repeat for other Criteria; for the number of characters, use a prompt statement. Google it. Note: prompt statements think of any value you provide is a string.
 //You will need to convert it to a number.
 
